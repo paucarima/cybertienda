ba92d34ce0ccc7461fa8911b67be255701fa443e
paucarima <80001362+paucarima@users.noreply.github.com>
2025-11-20 21:57:11 -0500

    dependencias de eslint

---
+++ env.js
@@ -0,0 +1,3 @@
+// Variables de entorno de Vite
+export const API_URL = import.meta.env.VITE_API_URL || undefined;
+export default API_URL;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { API_URL } from "../env"
 
 
-function App() {
+function App({ name }) {
+  console.log('import.meta.env =', import.meta.env)
+  console.log('import.meta.env.VITE_API_URL =', import.meta.env.VITE_API_URL)
+  console.log('API_URL (from env.js) =', API_URL)
+
+  const api = API_URL ?? '<<sin valor>>'
 
   return (
-    <h1>Hello Vite + React! {count}</h1>
+    <h1 style={{ color: 'blue' }}>Hola {api} </h1>
   )
 }
 
